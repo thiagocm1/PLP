f293810ba26c6dd0ccdb77cba889e1368fa44bec
thiagocm1 <30806616+thiagocm1@users.noreply.github.com>
2017-11-24 13:33:15 -0300

    Update blits.hs

--- Haskell/blits.hs
+++ Haskell/blits.hs
@@ -1,4 +1,10 @@
 blitz:: Int -> Int -> Double -> Bool
-blitz li dias bafo| li >= 30 || dias >= 30 || bafo >= 0.05 = True
-				  | li < 30 || dias < 30 || bafo < 0.05 = False
-				  
+blitz li dias bafo
+   | li >= 30 || dias >= 30 || bafo > 0.05 = True
+   | li < 30 || dias < 30 || bafo <= 0.05 = False
+
+main = do 
+   li <- getLine
+   dias <- getLine
+   bafo <- getLine
+   print (blitz (read li) (read dias) (read bafo))
